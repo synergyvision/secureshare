41d7d21fa128805e6f67c21fc98b2ff2bbec0a65
Luis <lugaliguori@gmail.com>
2019-04-17 10:29:39 -0400

    user search

--- app/dashboard/profile/contacts/contacts.js
+++ app/dashboard/profile/contacts/contacts.js
@@ -7,10 +7,16 @@
       templateUrl: '../dashboard/profile/contacts/contacts.html',
       controller: 'contactsController',
       css: 'contacts.css'
-    })
+    });
+    $stateProvider.state('dash.searchContacts', {
+      url: '/contacts/search',
+      templateUrl: '../dashboard/profile/contacts/search.html',
+      controller: 'contactsController',
+      css: 'contacts.css'
+      })
   }])
 
-  .controller('contactsController', function($scope,$http,$localStorage,$state,$window){
+  .controller('contactsController', function($scope,$http,$localStorage,$state,$window,$location){
       uid = $localStorage.uid;
 
       $scope.getContacts = function (){
@@ -32,5 +38,29 @@
         })
 
       }
+
+      $scope.getUsers = function (){
+            $scope.search = $localStorage.search
+            $http({
+                url: 'https://sharekey.herokuapp.com/contacts/users',
+                method: 'GET'
+            }).then(function (response){
+                if (response.data.status == 200){
+                    $scope.users = response.data.data;
+                    delete $localStorage.search;
+                }
+            }).catch(function (error){
+                if (error){
+                  if (error.status == 401){
+                      alert('Su sesion ha vencido por inactividad')
+                      $location.path('/login');
+                  }
+                  else{
+                      console.log(error.code);
+                      console.log(error.message);
+                  }
+                }  
+            }) 
+    }
 
   })
