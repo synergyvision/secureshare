1b6487562ae31cb3ce33603af2efe050fbb410e1
Luis <lugaliguori@gmail.com>
2019-07-08 17:47:48 -0400

    finished creating and answering surveys

---
+++ dashboard/surveys/surveys.js
@@ -0,0 +1,157 @@
+angular.module('sharekey.surveys', ['ui.router'])
+  
+  .config(['$stateProvider', function($stateProvider) {
+    $stateProvider.state('dash.surveys', {
+      url: '/surveys',
+      templateUrl: 'dashboard/surveys/surveys.html',
+      controller: 'surveyController',
+      css: 'surveys.css'
+    })
+    $stateProvider.state('dash.survey', {
+        url: '/surveys/?surveyId',
+        templateUrl: 'dashboard/surveys/survey.html',
+        controller: 'surveyController',
+        css: 'surveys.css'
+      })
+    $stateProvider.state('dash.newSurvey',{
+      url: '/surveys/newSurvey',
+      templateUrl: 'dashboard/surveys/newSurvey.html',
+      controller: 'surveyController',
+      css: 'surveys.css'
+    })
+  }])
+
+  .controller('surveyController', function($scope,$http,$localStorage,$state,$window,$sessionStorage,$stateParams,__env){
+    $scope.uid = $localStorage.uid;
+    var token = $localStorage.userToken;
+    survey = $stateParams.surveyId;
+
+    $scope.answers = [
+        {},
+        {}
+    ];
+
+    $scope.answeredQuestions = []
+
+    $scope.getSurveys = function (){
+        $http({
+            url: __env.apiUrl + __env.surveys,
+            method: 'GET',
+            headers: {'Authorization':'Bearer: ' + token}
+        }).then(function (response){
+            console.log(response.data.data)
+            $scope.surveys = response.data.data;
+        }).catch(function (error){
+            console.log(error)
+        })
+    }
+
+    $scope.getSurvey = function (){
+        $http({
+            url: __env.apiUrl + __env.surveys + survey,
+            method: 'GET',
+            headers: {'Authorization':'Bearer: ' + token}
+        }).then(function (response){
+            console.log(response.data)
+            $scope.survey = response.data;
+        }).catch(function (error){
+            console.log(error)
+        })
+    }
+
+    var createAnswers = function (surveyId,questionId) {
+        var newAnswers = $.param({
+            content: JSON.stringify($scope.answers)
+        })
+        console.log(newAnswers);
+        $http({
+            url: __env.apiUrl + __env.surveys + surveyId +'/question/' + questionId + '/answer',
+            method: 'POST',
+            data: newAnswers,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8','Authorization':'Bearer: ' + token}
+        }).then(function (response){
+            console.log(response.data)
+            $state.go('dash.surveys');
+        }).catch(function (error){
+            console.log(error)
+        })
+    }
+
+    var createQuestion = function (surveyId){
+        newQuestion = $.param({
+            content: $scope.question.title
+        })
+        $http({
+            url: __env.apiUrl + __env.surveys + surveyId +'/question',
+            method: 'POST',
+            data: newQuestion,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8','Authorization':'Bearer: ' + token}
+        }).then(function (response){
+            console.log(response.data)
+            createAnswers(surveyId,response.data.id_question)
+        }).catch(function (error){
+            console.log(error)
+        })
+
+    }
+
+    $scope.createSurvey = function (){
+        var newSurvey = $.param({
+            title: $scope.surveyTitle,
+            id_user: uid
+        })
+        $http({
+            url: __env.apiUrl + __env.surveys,
+            method: 'POST',
+            data: newSurvey,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8','Authorization':'Bearer: ' + token}
+        }).then(function (response){
+            surveyId = response.data.key;
+            console.log('created survey')
+            createQuestion(surveyId);
+        }).catch(function (error){
+            console.log(error)
+        })
+    }
+
+    $scope.addLenght =  function (){
+        answer = {};
+        $scope.answers.push(answer)
+    }
+
+    $scope.getId = function (questionId,answer){
+        var exists = false;
+        answeredQuestion = {
+            questionId: questionId,
+            answerId: answer.id
+        }
+        for (i =0; i < $scope.answeredQuestions.length;i++){
+            if ($scope.answeredQuestions[i].questionId == questionId){
+                $scope.answeredQuestions[i] = answeredQuestion 
+                exists = true
+            }
+        }
+        if (!exists){
+            $scope.answeredQuestions.push(answeredQuestion);
+        }
+    }
+
+    $scope.fillSurvey = function (){
+        console.log($scope.answeredQuestions[0]);
+        for (i = 0; i < $scope.answeredQuestions.length;i++){
+            $http({
+                url: __env.apiUrl + __env.surveys + survey +'/question/' + $scope.answeredQuestions[i].questionId + '/answer/' + $scope.answeredQuestions[i].answerId + '/vote',
+                method: 'PUT',
+                headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8','Authorization':'Bearer: ' + token}
+            }).then(function (response){
+                console.log(response.data);
+                $state.go('dash.surveys');
+            }).catch(function (error){
+                console.log(error);
+            })
+
+        }
+    }
+
+
+  })
