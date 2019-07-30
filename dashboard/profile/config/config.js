09e36cc4cb082aff4208c34b09625f0222e95979
Luis <lugaliguori@gmail.com>
2019-07-30 12:13:13 -0400

    testing validation message

--- dashboard/profile/config/config.js
+++ dashboard/profile/config/config.js
@@ -9,80 +9,11 @@ angular.module('sharekey.config', ['ngRoute','ui.router'])
   })
 }])
 
-.run(['$rootScope','$window',
-  function($rootScope,$window) {
-    console.log('aca')
-    $window.fbAsyncInit = function() {
-      // Executed when the SDK is loaded
-      FB.init({
-  
-        /*
-         The app id of the web app;
-         To register a new app visit Facebook App Dashboard
-         ( https://developers.facebook.com/apps/ )
-        */
-  
-        appId: '355312722034019',
-  
-        /*
-         Adding a Channel File improves the performance
-         of the javascript SDK, by addressing issues
-         with cross-domain communication in certain browsers.
-        */
-  
-        channelUrl: 'app/channel.html',
-  
-        /*
-         Set if you want to check the authentication status
-         at the start up of the app
-        */
-  
-        status: true,
-  
-        /*
-         Enable cookies to allow the server to access
-         the session
-        */
-  
-        cookie: true,
-  
-        /* Parse XFBML */
-  
-        xfbml: true,
-
-        version: 'v2.4'
-      })
-  
-    };
-  
-    (function(d){
-      // load the Facebook javascript SDK
-      var js,
-      id = 'facebook-jssdk',
-      ref = d.getElementsByTagName('script')[0];
-  
-      if (d.getElementById(id)) {
-        return;
-      }
-  
-      js = d.createElement('script');
-      js.id = id;
-      js.async = true;
-      js.src =  "https://connect.facebook.net/es_LA/sdk.js";
-  
-      ref.parentNode.insertBefore(js, ref);
-  
-    }(document));
-  
-
-}])
-
-.controller('configController', function($scope,$http,$localStorage,$state,$location,$stateParams,$location,$rootScope){
+.controller('configController', function($scope,$http,$localStorage,$state,$location,$stateParams,$location,$rootScope,$window){
     var token = $localStorage.userToken;
     var uid = $localStorage.uid;
     var username = $localStorage[uid + '-username'];
-    $scope.showFacebook = false;
-    $scope.validationMessage = 'Verificando. Soy ' + username + ' en Sharekey ';
+    var preMessage = 'Verificando. Soy ' + username + ' en Sharekey ';
   
     function makeid() {
       var result           = '';
@@ -94,19 +25,81 @@ angular.module('sharekey.config', ['ngRoute','ui.router'])
       return result;
    }
 
-    $scope.initObserver = function (){
-      $scope.showFacebook = true
-      FB.Event.subscribe('auth.authResponseChange', function(res) {
-        if (res.status === 'connected') {
-          console.log(res)
-          $scope.faceLogin = true
-          $scope.validationMessage= $scope.validationMessage + makeid();
-          $scope.$apply();
-        } else {
-          alert('Not Connected');
-        }
-      });
+   var initObserver = function (){
+    FB.Event.subscribe('auth.authResponseChange', function(res) {
+      if (res.status === 'connected') {
+        console.log(res)
+        $scope.faceLogin = true
+        $scope.validationMessage = preMessage + makeid();
+        $scope.$apply();
+      } else {
+        alert('Not Connected');
+      }
+    });
+  }
+
+  $window.fbAsyncInit = function() {
+    // Executed when the SDK is loaded
+    FB.init({
+
+      /*
+       The app id of the web app;
+       To register a new app visit Facebook App Dashboard
+       ( https://developers.facebook.com/apps/ )
+      */
+
+      appId: '355312722034019',
+
+      /*
+       Adding a Channel File improves the performance
+       of the javascript SDK, by addressing issues
+       with cross-domain communication in certain browsers.
+      */
+
+      channelUrl: 'app/channel.html',
+
+      /*
+       Set if you want to check the authentication status
+       at the start up of the app
+      */
+
+      status: true,
+
+      /*
+       Enable cookies to allow the server to access
+       the session
+      */
+
+      cookie: true,
+
+      /* Parse XFBML */
+
+      xfbml: true,
+
+      version: 'v2.4'
+    })
+
+    initObserver();
+  };
+
+  (function(d){
+    // load the Facebook javascript SDK
+    var js,
+    id = 'facebook-jssdk',
+    ref = d.getElementsByTagName('script')[0];
+
+    if (d.getElementById(id)) {
+      return;
     }
+
+    js = d.createElement('script');
+    js.id = id;
+    js.async = true;
+    js.src =  "https://connect.facebook.net/es_LA/sdk.js";
+
+    ref.parentNode.insertBefore(js, ref);
+
+  }(document));
 
     $scope.verify = function (){
         FB.api(
